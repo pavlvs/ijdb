fd2e599846349d7843610644ed9c9eac7d248721
Paul J Shaw <pavlvsxavier@gmail.com>
2020-07-05 16:01:52 +0100

    added EntryPoint

--- public/index.php
+++ public/index.php
@@ -11,49 +11,13 @@ function loadTemplate($templateFileName, $variables = []) {
 }
 
 try {
-    include __DIR__ . '/../includes/DatabaseConnection.php';
-    include __DIR__ . '/../classes/DatabaseTable.php';
-
-    $jokesTable = new DatabaseTable($db, 'jokes', 'id');
-    $authorsTable = new DatabaseTable($db, 'authors', 'id');
+    include __DIR__ . '/../classes/EntryPoint.php';
 
     //if no route variable is set, use 'joke/home'
     $route = ltrim(strtok($_SERVER['REQUEST_URI'], '?'), '/');
 
-    if ($route === strtolower($route)) {
-        if ($route === 'joke/list') {
-            include __DIR__ . '/../classes/controllers/JokeController.php';
-            $controller = new JokeController($jokesTable, $authorsTable);
-            $page = $controller->list();
-        } elseif ($route === '') {
-            include __DIR__ . '/../classes/controllers/JokeController.php';
-            $controller = new JokeController($jokesTable, $authorsTable);
-            $page = $controller->home();
-        } elseif ($route === 'joke/edit') {
-            include __DIR__ . '/../classes/controllers/JokeController.php';
-            $controller = new JokeController($jokesTable, $authorsTable);
-            $page = $controller->edit();
-        } elseif ($route === 'joke/delete') {
-            include __DIR__ . '/../classes/controllers/JokeController.php';
-            $controller = new JokeController( $jokesTable, $authorsTable);
-            $page = $controller->delete();
-        } elseif ($route === 'register') {
-            include __DIR__ . '/../classes/controllers/RegisterController.php';
-            $controller = new RegisterController($authorsTable);
-            $page = $controller->showForm();
-        }
-    } else {
-        http_response_code(301);
-        header('location: index.php?route=' . strtolower($route));
-    }
-
-    $title = $page['title'];
-
-    if (isset($page['variables'])) {
-        $output = loadTemplate($page['template'], $page['variables']);
-    } else {
-        $output = loadTemplate($page['template']);
-    }
+    $entryPoint = new EntryPoint($route);
+    $entryPoint->run();
 } catch (PDOException $e) {
     $title = 'An error has occurred';
 
